4a1c36e29acecf164e039fa55e912c22b06b7ec6
tdhoor <info@thomasdorfer.com>
2023-04-08 15:05:27 +0200

    updated delete all db entries function to mssql

--- src/functions/delete-all-entities.function.ts
+++ src/functions/delete-all-entities.function.ts
@@ -1,7 +1,14 @@
 import { DB } from "src/db";
 
+let notFirstCall = false;
+
 export async function deleteAllEntities() {
     const entities = DB.entityMetadatas;
-    const tableNames = entities.map((entity) => `"${entity.tableName}"`).join(", ");
-    await DB.manager.query(`TRUNCATE ${tableNames} RESTART IDENTITY CASCADE;`);
+    for (const entity of entities) {
+        if (notFirstCall) {
+            await DB.query(`DELETE from [${entity.tableName}];`)
+            await DB.query(`DBCC CHECKIDENT ([${entity.tableName}], RESEED, 0);`);
+        }
+    }
+    notFirstCall = true;
 }
