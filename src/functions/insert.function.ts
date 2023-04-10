c25b2f3889d9ce4ae140b690e5f18a35439a65ac
tdhoor <info@thomasdorfer.com>
2023-04-10 14:34:57 +0200

    added bulk insert for mysql function

--- src/functions/insert.function.ts
+++ src/functions/insert.function.ts
@@ -1,5 +1,13 @@
 import { DB } from "../db";
+import mysql from "mysql2";
 
 export function insert<T>(type: (new () => T), data: any[]) {
-    return DB.manager.save(type, data, { chunk: 10000 });
+    const pool = mysql.createPool({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
+    const tableName = DB.manager.getRepository(type).metadata.tableName;
+    return bulkInsertMysql(pool, tableName, data);
 }
