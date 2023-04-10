af3101401b1d8617ba0fdc228117ed65a7f46fbf
tdhoor <info@thomasdorfer.com>
2023-04-10 21:51:17 +0200

    updated bulk method

--- src/functions/insert.function.ts
+++ src/functions/insert.function.ts
@@ -1,5 +1,9 @@
 import { DB } from "../db";
 
 export function insert<T>(type: (new () => T), data: any[]) {
-    return DB.manager.save(type, data, { chunk: 10000 });
+    return DB.manager.createQueryBuilder()
+        .insert()
+        .into(type)
+        .values(data)
+        .execute()
 }
