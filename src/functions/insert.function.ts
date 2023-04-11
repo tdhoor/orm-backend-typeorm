fe6359659d5a36e09528e61bdc00c151278a336d
tdhoor <info@thomasdorfer.com>
2023-04-11 15:06:29 +0200

    updated core lib and adjusted inser function to it

--- src/functions/insert.function.ts
+++ src/functions/insert.function.ts
@@ -1,7 +1,8 @@
 import { DB } from "../db";
-import { bukdInsertMssql } from "@core/functions/bulk-insert-mssql.function";
+import { bulkInsertMssql } from "@core/functions/bulk-insert-mssql.function";
+import sql from "mssql";
 
 export async function insert<T>(type: (new () => T), data: any[]) {
     const table = DB.manager.connection.getMetadata(type).tableName;
-    await bukdInsertMssql(table, data);
+    await bulkInsertMssql(sql, table, data);
 }
