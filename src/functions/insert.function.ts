8f3babffa5c5bb462ba09d6bcf9ce4342ca2b5d3
tdhoor <info@thomasdorfer.com>
2023-04-17 11:32:15 +0200

    added request timeout to mssql pool

--- src/functions/insert.function.ts
+++ src/functions/insert.function.ts
@@ -3,6 +3,8 @@ import { bulkInsertMssql } from "@core/functions/bulk-insert-mssql.function";
 import sql from "mssql";
 
 export async function insert<T>(type: (new () => T), data: any[]) {
+    console.log("Inserting data into table: " + DB.manager.connection.getMetadata(type).tableName + " length: " + data.length);
     const table = DB.manager.connection.getMetadata(type).tableName;
     await bulkInsertMssql(sql, table, data);
+    console.log("\tInserted data into table: " + DB.manager.connection.getMetadata(type).tableName + " length: " + data.length);
 }
